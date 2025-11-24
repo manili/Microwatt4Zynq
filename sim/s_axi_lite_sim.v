6e02ecfd0cd4cb578a91a0a3ba43eb3b888281a2
Mohammad A. Nili <manili.devteam@gmail.com>
2025-11-24 17:55:52 -0500

    Initial commit

--- sim/s_axi_lite_sim.v
+++ sim/s_axi_lite_sim.v
@@ -1,30 +1,3 @@
-/*
- * Copyright 2025 Mohammad A. Nili
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Module: s_axi_lite_sim
- *
- * Description:
- *   Fixed AXI4-Lite slave
- *   - Latches AWADDR and WDATA/WSTRB separately and performs the write only
- *     when both address and data have been accepted (handles either order).
- *   - Proper read address latching.
- *   - Active-low synchronous reset (aresetn).
- *   - Single-beat AXI-Lite only.
- */
 `timescale 1ns/1ps
 
 module s_axi_lite_sim #(
